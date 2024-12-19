5f2f72c6816771e7190227eb0edeec25391bd784
Gareth <gareth.james@force24.co.uk>
2024-12-19 08:00:15 +0000

    setup complete

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
 
-with open("./data.csv", "r").read() as data:
+items = []
+
+with open("./data.csv", "r", encoding="utf-8") as data:
     #your code here
-    
-
-
+    pass
     #end of your code here
+    
+with open("./output.json", "w") as file:
+    var_to_dump = {"items": items}
+
+    json.dump(var_to_dump, file, ensure_ascii=False)
